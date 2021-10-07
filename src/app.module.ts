b243056f3c9d4b6bffc9cfe78c3fff79e4ac37aa
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-10-07 09:30:12 +0200

    registering CommonController + ProductsController + ProductsService on AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommonController } from './common/common.controller';
+import { ProductsController } from './products/products.controller';
+import { ProductsService } from './products/products.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CommonController, ProductsController],
+  providers: [AppService, ProductsService],
 })
 export class AppModule {}
