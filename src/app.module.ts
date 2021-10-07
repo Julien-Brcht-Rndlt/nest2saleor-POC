f1b8201c99aa28180e188624d6eba534c7d3adcf
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-10-07 15:32:08 +0200

    setting app configuration with external .env file (using ConfigModule)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { CommonController } from './common/common.controller';
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, CommonController, ProductsController],
   providers: [AppService, ProductsService],
 })
