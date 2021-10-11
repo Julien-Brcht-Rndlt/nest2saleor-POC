90dfb235124be072ea50acdd23cc0c2ef8200dcd
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-10-11 15:08:58 +0200

    adding doc comment for applyXAccessToken description

--- src/tools/common-funcs.ts
+++ src/tools/common-funcs.ts
@@ -1,5 +1,11 @@
 import { Request } from '@nestjs/common';
 
+/**
+ * Helper tool function that allows to apply auth x-access-token onto another final performed action
+ * @param request Request object
+ * @param callback callback that calls the final performed action
+ * @returns
+ */
 export const applyXAccessToken = <T = any>(
   request: Request,
   callback: (t: string) => T,
